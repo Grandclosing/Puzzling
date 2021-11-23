ec902ae8825f8552dd795bcf510e936b9db269af
Grandclosing <vukpetrovic1@yahoo.com>
2021-11-22 21:56:24 -0500

    Fixed bug in unique string check function. Finished tests for puzzle 1. Added extra checks for encoding.

--- src/p_001.ts
+++ src/p_001.ts
@@ -42,12 +42,19 @@ const contains_unique_chars_in_place = (input : string) : boolean => {
 export const contains_unique_chars = (input : string, encoding : string, inPlace : boolean) : boolean => {
     if(!input) throw new Error('Expecting valid, non-empty string input');
     if(input.length == 1) return true;
-
-    if(encoding == 'ascii' && input.length > 256)
-        return true;
-    else if(encoding == 'unicode' && input.length > 1111998)
-        // based on https://stackoverflow.com/a/5928054/3578493
-        return true;
+    
+    switch(encoding) {
+        case 'ascii': 
+            if(input.length > 256)
+                return false;
+            break;
+        case 'unicode': 
+            // based on https://stackoverflow.com/a/5928054/3578493
+            if(input.length > 1111998)
+                return false;
+            break;
+        default: throw new Error(`Unsupported encoding: ${encoding}`);
+    }
 
     return inPlace ? contains_unique_chars_in_place(input) : contains_unique_chars_simple(input);
 }
--- test/test_p001.js
+++ test/test_p001.js
@@ -6,8 +6,48 @@ describe('Puzzle 1', () => {
     describe('#contains_unique_chars()', () => {
         it('Should throw an exception when an empty, null, or undefined string is passed in', () => {
             expect(() => {
-                contains_unique_chars("", 'utf-8', true);
+                contains_unique_chars("", 'ascii', true);
             }).to.throw('Expecting valid, non-empty string input');
+
+            expect(() => {
+                contains_unique_chars(null, 'ascii', true);
+            }).to.throw('Expecting valid, non-empty string input');
+
+            expect(() => {
+                contains_unique_chars(undefined, 'ascii', true);
+            }).to.throw('Expecting valid, non-empty string input');
+        });
+
+        it('Should throw an exception when passed in an encoding not one of ascii or unicode', () => {
+            expect(() => {
+                contains_unique_chars("abc", 'ascii', true);
+            }).to.not.throw();
+
+            expect(() => {
+                contains_unique_chars("abc", 'unicode', true);
+            }).to.not.throw();
+
+            expect(() => {
+                contains_unique_chars("abc", 'ISO 8859-5', true);
+            }).to.throw('Unsupported encoding: ISO 8859-5');
+        });
+
+        it('Should return true for completely unique strings', () => {
+            expect(contains_unique_chars('a', 'ascii', true)).to.be.true;
+
+            expect(contains_unique_chars('abcdefg', 'ascii', true)).to.be.true;
+
+            expect(contains_unique_chars('qwertyuiop[]1234567890-=!@#$%^&*()_+', 'ascii', true)).to.be.true;
+        });
+
+        it('Should return false for non-unique strings', () => {
+            expect(contains_unique_chars('aa', 'ascii', true)).to.be.false;
+
+            expect(contains_unique_chars('abcdefgha', 'ascii', true)).to.be.false;
+
+            expect(contains_unique_chars('qwertyuiop[]1234567890-=!@#$%^&*()_+aa', 'ascii', true)).to.be.false;
+
+            expect(contains_unique_chars('qwertyuiop[]123456a7a890-=!@#$%^&*()_+', 'ascii', true)).to.be.false;
         });
     });
 });
